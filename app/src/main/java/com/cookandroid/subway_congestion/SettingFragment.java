cf8ab098a2ef5964a288cc96763163235b15d2fa
congtongc <congsai1991@gmail.com>
2023-05-27 02:27:07 +0900

    지금까지 만든 코드(23.05.25) - 리스트뷰 검색 - 메뉴 - 하단 탭 - 프래그먼트 3개 - 역 리스트 Json 파일로 등록 - 역 리스트 더블 클릭 시 즐겨찾기 항목에 추가 - 즐겨찾기 프래그먼트에 즐겨찾기 항목 표시 - 즐겨찾기 항목 더블 클릭 시 삭제 - 앱 종료 시에도 즐겨찾기 항목 저장 - 이미 추가된 즐겨찾기 항목 중복 방지 - 설정 프래그먼트에 앱 종료 버튼 설정

--- app/src/main/java/com/cookandroid/subway_congestion/SettingFragment.java
+++ app/src/main/java/com/cookandroid/subway_congestion/SettingFragment.java
@@ -1,32 +1,37 @@
 package com.cookandroid.subway_congestion;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
 public class SettingFragment extends Fragment {
 
-    private Button settingButton;
+    private ImageButton closebtn;
 
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
 
-        settingButton = view.findViewById(R.id.settingButton);
-        settingButton.setOnClickListener(new View.OnClickListener() {
+        closebtn = view.findViewById(R.id.CloseBtn);
+        closebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 설정 기능 구현
-                // 버튼을 클릭했을 때 동작할 코드 작성
+                if (Build.VERSION.SDK_INT >= 21) {
+                    getActivity().finishAndRemoveTask();
+                } else {
+                    getActivity().finish();
+                }
+                System.exit(0);
             }
         });
-
         return view;
     }
 }
